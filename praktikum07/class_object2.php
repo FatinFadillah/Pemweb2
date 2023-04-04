7eca97051d3a9a1429e903a2c62a09b36b18023f
FatinFadillah <115238757+FatinFadillah@users.noreply.github.com>
2023-04-04 19:06:57 +0700

    Add files via upload

---
+++ praktikum07/class_object2.php
@@ -0,0 +1,29 @@
+<?php
+class Buah
+{
+    public $warnaKulit;
+    public $rasa;
+    public $aroma;
+
+    function __construct($nama, $warna, $taste, $bau)
+    {
+        $this->namaBuah = $nama;
+        $this->warnaKulit = $warna;
+        $this->rasa = $taste;
+        $this->aroma = $bau;
+    }
+
+    function tumbuh()
+    {
+        return "Buah $this->namaBuah itu tumbuh !";
+    }
+
+    function busuk()
+    {
+        return "Buah $this->namaBuah itu busuk !";
+    }
+}
+$objek = new Buah("Sirsak", "Merah", "Manis", "Harum");
+echo $objek->tumbuh();
+echo "<br>";
+echo $objek->busuk();
